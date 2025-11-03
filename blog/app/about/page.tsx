4272ea6825d2172f4e391ee48fb829eec213693a
dam33534@naver.com <dam33534@naver.com>
2025-11-03 17:47:34 +0900

    feat : 메인 페이지 UI 생성
    메인 페이지 UI 생성

--- blog/app/about/page.tsx
+++ blog/app/about/page.tsx
@@ -1,16 +1,10 @@
-import Header from '../../components/layouts/Header';
-import Footer from '../../components/layouts/Footer';
-
 export default function About() {
   return (
-    <div className="flex min-h-screen flex-col">
-      <Header />
-      <main className="flex-1">
-        <div className="container mx-auto px-4 py-8">
-          <h2 className="text-3xl font-bold tracking-tight">소개</h2>
-        </div>
-      </main>
-      <Footer />
+    <div className="container  py-8">
+      <div className="space-y-8">
+        {/* 섹션 제목 */}
+        <h2 className="text-3xl font-bold tracking-tight">소개</h2>
+      </div>
     </div>
   );
 }
---
+++ blog/app/docs/layout.tsx
@@ -0,0 +1,17 @@
+import { ReactNode } from 'react';
+
+interface DocsLayoutProps {
+  children: ReactNode;
+}
+
+export default function DocsLayout({children}: DocsLayoutProps) {
+  return (
+    <div className="container  py-8">
+      <div className="space-y-8">
+      {children}
+      </div>
+    </div>
+  );
+}
+
+
