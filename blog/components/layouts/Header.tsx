6ba928307e6f4d10147a892ba6b37ca76b382f0e
dam33534@naver.com <dam33534@naver.com>
2025-11-04 17:49:08 +0900

    feat : 노션 API 블로그 목록 연동 개발
    노션 API 블로그 목록 연동 개발

--- blog/components/layouts/Header.tsx
+++ blog/components/layouts/Header.tsx
@@ -4,7 +4,7 @@ export default function Header() {
 
   return (
     <header className="sticky top-0 z-50 border-b bg-background">
-      <div className="container flex h-14 items-center px-4">
+      <div className="container flex h-[var(--header-height)] items-center px-4">
         <Link href="" className="text-xl font-semibold">
           <span className="font-bold">블로그</span>
         </Link>
